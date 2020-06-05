85616de5e3f64db47e8be42341673129e11e246b
Evan Campion <evancn7@gmail.com>
2020-06-05 15:01:27 +0100

    Improved the main script so it pulls info more information.
    compile_data function now outputs two sorted dictionaries
    user_stats dictionary included a list so now all user stats are in one
    dicionary as opposed to two
    list includes info & error counts as the two value

--- ticky_check.py
+++ ticky_check.py
@@ -5,55 +5,54 @@ import operator
 import csv
 
 
-def organise_data():
-	error = {}
-	per_user_error = {}
-	per_user_info = {}
-	with open('syslog.log') as logfile:
-		for line in logfile.readlines():
-			username = re.search(r'\((.*)\)', line).group(1)
-			result_error = re.search(r'ticky: ERROR ([\w ]*) ', line)
-			result_info = re.search(r'ticky: INFO ([\w ]*) ', line)
-			if result_error != None:
-				error[result_error.group(1)] = error.get(result_error.group(1), 0) + 1
-				per_user_error[username] = per_user_error.get(username, 0) + 1
-			if result_info != None:
-				per_user_info[username] = per_user_info.get(username, 0) + 1
-	return error, per_user_error, per_user_info
+def compile_data():
+	errors = {}
+	user_stats = {}
+	with open('syslog.log') as file:
+		for line in file.readlines():
+			result = re.search(r"ticky: ([\w]*) ([\w ']*).*\(([\w. ]*)\)", line)
+			log_type = result.group(1)
+			log_detail = result.group(2)
+			username = result.group(3)
+			if log_type == 'INFO':
+				val = user_stats.get(username, [0, 0])
+				val[0] += 1
+				user_stats[username] = val
+			if log_type == 'ERROR':
+				val = user_stats.get(username, [0, 0])
+				val[1] += 1
+				user_stats[username] = val
+				errors[log_detail] = errors.get(log_detail, 0) + 1
+	return sort_errors(errors), sort_users(user_stats)
 
 
-def sort_data(data):
-	error, per_user_error, per_user_info = data
-	error_sorted = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
-	per_user_error_sorted = dict(sorted(per_user_error.items()))
-	per_user_info_sorted = dict(sorted(per_user_info.items()))
-	usernames = list(per_user_error_sorted)
-	usernames.extend(list(per_user_info_sorted))
-	username_list = sorted(set(usernames))
-	return error_sorted, per_user_error_sorted, per_user_info_sorted, username_list
+def sort_errors(dictionary):
+	"""takes in dictionary sorts it in descending order on values and returns tuple"""
+	result = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
+	result.insert(0, ('Error', 'Count'))
+	return result
 
 
-def write_data_csv(error, per_user_error, per_user_info, username_list):
-	# writing the errors to a csv file with headers included
-	with open('error_message.csv', mode='w') as file:
-		fieldnames = ['Error', 'Count']
-		writer = csv.DictWriter(file, fieldnames=fieldnames)
-		writer.writeheader()
-		for key in error:
-		       writer.writerow({'Error': key[0], 'Count': key[1]})
+def sort_users(dictionary):
+	"""takes in dictionary sorts it by key in alphabetical order and returns tuple"""
+	result = sorted(dictionary.items())
+	result.insert(0, ('Username', ['INFO', 'ERROR']))
+	return result
 
 
-	with open('user_statistics.csv', mode='w') as file:
-		fieldnames = ["Username", "INFO", "ERROR"]
-		writer = csv.DictWriter(file, fieldnames=fieldnames)
-		writer.writeheader()
-		usernames = username_list
-		for username in username_list:
-			writer.writerow({'Username': username, 'INFO': per_user_info.get(username, 0),
-				'ERROR': per_user_error.get(username, 0)})
+def write_csv(errors, user_stats):
+		with open('error_message.csv', mode='w') as file:
+			writer = csv.writer(file)
+			for data in errors:
+				writer.writerow(data)
+		with open('user_statistics.csv', mode='w') as file:
+			writer = csv.writer(file)
+			for data in user_stats:
+				writer.writerow([data[0], data[1][0], data[1][1]])
 
 
 if __name__ == '__main__':
-		data = organise_data()
-		error_sorted, per_user_error_sorted, per_user_info_sorted, username_list = sort_data(data)
-		write_data_csv(error_sorted, per_user_error_sorted, per_user_info_sorted, username_list)
+	errors = compile_data()[0]
+	user_stats = compile_data()[1]
+	write_csv(errors, user_stats)
+
